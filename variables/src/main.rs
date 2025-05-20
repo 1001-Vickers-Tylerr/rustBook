81b67b13170ccf8006ad693ec262e9487b537e25
VickersCode <tyler31vickers@gmail.com>
2025-05-20 12:45:09 -0700

    testing shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,23 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    // Shadowing using let
+
+    let y = 5;
+
+    let y = y + 1;
+    println!("The value of y is: {y}"); // outputs 6
+
+    {
+        let y = y * 2;
+        println!("The value of y is: {y}"); // outputs 12
+    }
+
+    // Now, because of curly brackets (scope),
+    // if we call println! again, it will output 6
+
+    println!("The value of y is: {y}");
 }
+
+
