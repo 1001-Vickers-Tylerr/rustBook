8dc18b4f36d3dca3a66094abe785e6a32e74e8c0
VickersCode <tyler31vickers@gmail.com>
2025-05-20 12:28:26 -0700

    testing muttability

---
+++ variables/target/debug/deps/variables-04d50ce308428d9f.d
@@ -0,0 +1,5 @@
+/home/vickersschool/rustBook/variables/target/debug/deps/variables-04d50ce308428d9f: src/main.rs
+
+/home/vickersschool/rustBook/variables/target/debug/deps/variables-04d50ce308428d9f.d: src/main.rs
+
+src/main.rs:
