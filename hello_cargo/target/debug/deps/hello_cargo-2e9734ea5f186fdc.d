fb3d25aa45d79eb0beae302cbeeb5f8683c60fe2
VickersCode <tyler31vickers@gmail.com>
2025-05-20 10:14:25 -0700

    testing gitignore

---
+++ hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc.d
@@ -0,0 +1,5 @@
+/home/vickersschool/rustBook/hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc: src/main.rs
+
+/home/vickersschool/rustBook/hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/vickersschool/rustBook/hello_cargo/target/debug/hello_cargo: /home/vickersschool/rustBook/hello_cargo/src/main.rs
